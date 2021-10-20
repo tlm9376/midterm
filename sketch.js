89a1f9848c1ba24dc5029c549307528e764550a4
tlm9376 <90855941+tlm9376@users.noreply.github.com>
2021-10-20 10:47:29 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+let face = [];
+
+let nose = [nose1,nose2,nose3];
+let numNoses;
+let lips = [];
+let eyebrows = []
+let eyes = [];
+let noses = []
+let lips = []
+let faceShape = []
+let skinTone = []
+let hair = []
+let hairColor = []
+
+function setup() {
+  createCanvas(400, 400);
+
+  for (let i = 0;i < numNoses,i++){
+    nose[i+=1]
+  }
+
+}
+
+function draw() {
+  background(220);
+}
